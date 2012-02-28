066ed1c829c68837a5fb238893c35760b81e162a
elizarov <elizarov@gmail.com>
2012-02-29 01:26:01 +0400

    Added standard devidation report and improved stability of result gathering code

--- IntListThroughput.java
+++ IntListThroughput.java
@@ -1,5 +1,5 @@
 import java.util.*;
-import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Phaser;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -12,10 +12,11 @@ public class IntListThroughput {
 	private static final int DURATION_SECS = 20;
 	private static final int AVG_FROM_SEC = 3;
 
+	private static Phaser phaser;
+	private static List<Test> tests;
+
 	static class Test implements Runnable {
 		final IntList list;
-		final CyclicBarrier startBarrier;
-		final CyclicBarrier stopBarrier;
 
 		final AtomicInteger counter = new AtomicInteger();
 
@@ -24,9 +25,7 @@ public class IntListThroughput {
 
 		volatile boolean done;
 
-		private Test(Class<?> implClass, int size, CyclicBarrier startBarrier, CyclicBarrier stopBarrier) throws Exception {
-			this.startBarrier = startBarrier;
-			this.stopBarrier = stopBarrier;
+		private Test(Class<?> implClass, int size) throws Exception {
 			list = (IntList)implClass.newInstance();
 			Random random = new Random(1);
 			for	(int i = 0; i < size; i++)
@@ -37,9 +36,9 @@ public class IntListThroughput {
 			try {
 				while (true) {
 					done = false;
-					startBarrier.await();
+					phaser.arriveAndAwaitAdvance();
 					test();
-					stopBarrier.await();
+					phaser.arriveAndAwaitAdvance();
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -67,59 +66,78 @@ public class IntListThroughput {
 	}
 
 	public static void main(String[] args) throws Exception {
-		if (args.length != 2) {
-			System.err.println("Usage: " + IntListIterationTiming.class + " <threads> <impl>");
-			System.err.println("Where: <threads> the number of threads.");
-			System.err.println("       <impl>    is one of: ");
+		if (args.length != 3) {
+			System.err.println("Usage: " + IntListIterationTiming.class + " <min-threads> <max-threads> <impl>");
+			System.err.println("Where: <min-threads> the minimal number of threads.");
+			System.err.println("       <max-threads> the maximal number of threads.");
+			System.err.println("       <impl>        is one of: ");
 			for (Class c : IntList.class.getDeclaredClasses())
 				System.err.println("                    " + c.getSimpleName());
 			return;
 		}
 
-		int threads = Integer.decode(args[0]);
-		Class<?> implClass = getImplClass(args[1]);
+		int minThreads = Integer.decode(args[0]);
+		int maxThreads = Integer.decode(args[1]);
+		Class<?> implClass = getImplClass(args[2]);
 
-		CyclicBarrier startBarrier = new CyclicBarrier(threads + 1);
-		CyclicBarrier stopBarrier = new CyclicBarrier(threads + 1);
-		List<Test> tests = new ArrayList<Test>(threads);
-		for (int i = 0; i < threads; i++) {
-			Test test = new Test(implClass, MAX_SIZE, startBarrier, stopBarrier);
-			Thread thread = new Thread(test);
-			thread.setDaemon(true);
-			thread.start();
-			tests.add(test);
-		}
+		phaser = new Phaser(1);
+		tests = new ArrayList<Test>(maxThreads);
 
-		Map<Integer, Double> results = new HashMap<Integer, Double>();
-		for (int size = MIN_SIZE; size <= MAX_SIZE; size *= 10) {
-			System.out.printf("--- Running test for size %,d: ", size);
-			for (Test test : tests)
-				test.size = size;
-			startBarrier.await();
+		for (int i = 0; i < maxThreads; i++)
+			tests.add(new Test(implClass, MAX_SIZE));
+		for (int i = 0; i < minThreads - 1; i++)
+			startThread(i);
 
-			long from = 0;
-			long to = 0;
-			for (int sec = 0; sec < DURATION_SECS; sec++) {
-				Thread.sleep(1000);
-				long total = 0;
-				for (int i = 0; i < threads; i++)
-					total += tests.get(i).counter.get();
-				if (sec == AVG_FROM_SEC)
-					from = total;
-				to = total;
-				System.out.print('.');
+		for (int threads = minThreads; threads <= maxThreads; threads++) {
+			startThread(threads - 1);
+			for (int size = MIN_SIZE; size <= MAX_SIZE; size *= 10) {
+				System.out.printf(Locale.US, "#%d [%,10d]: ", threads, size);
+				launchTest(threads, size);
+				Stats stats = gatherTestStats(threads, size);
+				stopTest(threads);
+				System.out.printf(Locale.US, " done %s x 10^9 ops/sec%n", stats);
 			}
-			double result = (to - from) * size / 1e9 / (DURATION_SECS - AVG_FROM_SEC);
-			System.out.printf(Locale.US, " done %.2f x 10^9 ops/sec%n", result);
-			results.put(size, result);
-
-			for (Test test : tests)
-				test.done = true;
-			stopBarrier.await();
 		}
+	}
 
-		for (int size = MIN_SIZE; size <= MAX_SIZE; size *= 10)
-			System.out.printf(Locale.US, "[%,10d] Average throughput: %.2f x 10^9 ops/sec%n", size, results.get(size));
+	private static void startThread(int i) {
+		phaser.register();
+		Thread t = new Thread(tests.get(i));
+		t.setDaemon(true);
+		t.start();
+	}
 
+	private static void launchTest(int threads, int size) {
+		for (int i = 0; i < threads; i++)
+			tests.get(i).size = size;
+		phaser.arriveAndAwaitAdvance();
+	}
+
+	private static Stats gatherTestStats(int threads, int size) throws InterruptedException {
+		Stats stats = new Stats();
+		for (int sec = 0; sec < DURATION_SECS; sec++) {
+			long prevCount = totalCount(threads);
+			long prevTime = System.nanoTime();
+			Thread.sleep(1000);
+			long count = totalCount(threads);
+			long time = System.nanoTime();
+			if (sec >= AVG_FROM_SEC)
+				stats.add((double)size * (count - prevCount) / (time - prevTime));
+			System.out.print('.');
+		}
+		return stats;
+	}
+
+	private static long totalCount(int threads) {
+		long total = 0;
+		for (int i = 0; i < threads; i++)
+			total += tests.get(i).counter.get();
+		return total;
+	}
+
+	private static void stopTest(int threads) {
+		for (int i = 0; i < threads; i++)
+			tests.get(i).done = true;
+		phaser.arriveAndAwaitAdvance();
 	}
 }
